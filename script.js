580318ce1b83c9bcbc6d9075e2696f375bebf430
Sayandeep Biswas <126375013+Itssayandeep@users.noreply.github.com>
2023-07-06 13:53:57 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,32 @@
+const endDate = "8 JULY, 2023 10:00 PM"
+
+document.getElementById("end-Date").innerText = endDate;
+const inputs = document.querySelectorAll("input")
+
+function clock(){
+    const end= new Date(endDate)
+    const now= new Date()
+    const diff = (end - now)/1000;
+    
+    if(diff < 0) return;
+    inputs[0].value=(Math.floor(diff/3600/24));
+    inputs[1].value=(Math.floor(diff/3600)%24);
+    inputs[2].value=(Math.floor(diff/60)%60);
+    inputs[3].value=(Math.floor(diff)%60);
+}
+
+clock()
+
+/**
+ * 1day=24hours
+ * 1hr=60 mins
+ * 60mins=3600s
+ */
+setInterval(
+    ()=>{
+        clock()
+    },
+    1000
+) 
+
+
